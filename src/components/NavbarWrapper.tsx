e738d63d67a68c6c0fe0ebbb1e3cd17e1d5d9f5b
dafdoftwo <ahmed@example.com>
2026-04-14 15:04:30 +0200

    fix: ENABLE SSR FOR NAVBAR TO DESTROY CLS, bump edge cache to v8

--- src/components/NavbarWrapper.tsx
+++ src/components/NavbarWrapper.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import dynamic from 'next/dynamic';
+import Navbar from './Navbar';
 import { usePathname } from 'next/navigation';
-
-const Navbar = dynamic(() => import('./Navbar'), { ssr: false });
 
 /** Pages that should render without the site-wide navbar */
 const STANDALONE_ROUTES = ['/advertise'];
